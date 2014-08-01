fd92e553f12b726e5450a110f336a42620a08c6c
mic1780 <mic1780@users.noreply.github.com>
2014-08-01 14:11:51 -0400

    Added workaround for mod security to create.js

--- includes/js/create.js
+++ includes/js/create.js
@@ -10,4 +10,22 @@ jQuery(function() {
 	jQuery('[name="newCode[Attributes]"]').on('change', function(e) {
 		jQuery(this).val( jQuery(this).val().toLowerCase().replace(/[^a-z\n\r]+/, '') );
 	});
+	
+	jQuery('[name="newCode[FunctionCode]"]').on('change', function(e) {
+		var tagsToChange = Array('script','noscript');
+		var regexTagsToChange =	'(?=' + tagsToChange.join('|') + ')';
+		var regPatt = new RegExp("<(\\/?)(?!')" + regexTagsToChange + "([^\\n\\r>]*)>", "g");
+		var shouldReplace;
+		
+		if ( jQuery(this).val().search(regPatt) === -1 ) {
+			shouldReplace = false;
+		} else {
+			shouldReplace = true;
+		}//END IF
+		
+		if (shouldReplace) {
+			jQuery(this).val( jQuery(this).val().replace(regPatt, "[$1$2]") );
+		}//END IF
+		
+	});
 });
